5b6ca81efd3af0c76b5fe5fb549b73e3d5e34577
Pavel Neprin <41507524+neprin@users.noreply.github.com>
2022-10-06 21:41:07 +0300

    Aurora Weather App

---
+++ Aurora/Model/WeatherData.swift
@@ -0,0 +1,23 @@
+//
+//  WeatherData.swift
+//  Aurora
+//
+//  Created by Pavel Neprin on 10/6/22.
+//
+
+import Foundation
+
+struct WeatherData: Codable {
+    let name: String
+    let main: Main
+    let weather: [Weather]
+}
+
+struct Main: Codable {
+    let temp: Double
+}
+
+struct Weather: Codable {
+    let description: String
+    let id: Int
+}
